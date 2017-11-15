1525c3903817535281db8267d6bb151b2e65c4ed
Artem <razzzor979797@gmail.com>
2017-11-15 22:36:00 +0500

    MinorAdjustment
    =)

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -14,6 +14,29 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+			
         }
+		public function createEverything(){
+			Label name = new Label();
+			name.Text = "The best project in the world!";
+			Label query = new Label();
+			query.Text = "Do you agree?";
+			Button yes = new Button();
+			yes.Text = "Yes";
+			Button no = new Button();
+			no.Text = "No";
+			name.Position.X = 10;
+			name.Position.Y = 10;
+			query.Position.X = 10;
+			query.Position.Y = 30;
+			yes.Position.X = 25;
+			yes.Position.Y = 50;
+			no.Position.X = 75;
+			no.Position.Y = 50;
+			this.Elements.Add(name);
+			this.Elements.Add(query);
+			this.Elements.Add(yes);
+			this.Elements.Add(no);
+		}
     }
 }
